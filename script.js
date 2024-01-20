5f43d3d03b9878944595faf3490d47b3a443d13a
Robert P. <porobertdev@gmail.com>
2024-01-20 10:45:03 +0200

    Allow to play the game and display the results
    * added a testBoard to make testing easier

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-const gameBoard = (function () {
+const gameBoard = (function (testBoard) {
 
     const rows = 3;
     const columns = 3;
-    const board = [];
+    
+    const board = (function() {
+        if (testBoard) {
+            return [
+                ['X', 'O', 'X'],
+                ['O', 'X', 'O'],
+                ['O', 'O', 'X']
+            ];
+        } else {
+            const arr = [];
 
-    // create rows * columns board
-    for (i = 0; i < rows; i++) {
-        board.push([]);
-        for (j = 0; j < columns; j++) {
-            board[i].push('');
+            // create rows * columns board
+            for (i = 0; i < rows; i++) {
+                arr.push([]);
+                for (j = 0; j < columns; j++) {
+                    arr[i].push('');
+                }
+            }
+            return arr;
         }
-    }
+    })();
 
     const getBoard = () => board;
 
+    const getEmptyCells = () => {
+        // checks if any row of the board has empty cells
+        return board.filter( row => row.includes('')).length;
+    }
+
     return {
-        getBoard
+        getBoard,
+        getEmptyCells
     };
-})();
+})(true);
 
 const gameController = (function() {
 
@@ -34,4 +52,26 @@ const gameController = (function() {
 
     // set first player
     let currPlayer = player1;
+
+    function playRound(row, cell) {
+        // row & cell required to play from the console
+
+        if (gameBoard.getEmptyCells() == 0) {
+            // game ends
+        } else {
+            // update player of the round
+            currPlayer = (currPlayer == player1) ? player2 : player1;
+            board[row][cell] = currPlayer.mark;
+        }
+
+        display(board);
+    }
+
+    return {playRound};
 })();
+
+function display(board) {
+    for (row of board) {
+        console.log(row.join(' | '));
+    }
+}
