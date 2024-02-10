dcb745d3da42e5967ddd0791acec8504237d212f
Robert P. <porobertdev@gmail.com>
2024-02-10 17:00:08 +0200

    Convert IIFE to classes
    * Just practicing.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const gameBoard = (function () {
+class GameBoard {
 
-    const rows = 3;
-    const columns = 3;
+    #rows = 3;
+    #columns = 3;
     
-    function createBoard(type) {
+    #createBoard(type) {
 
         const arr = (type == 'console') ? [] : Array.from(document.querySelectorAll('.cell'));
 
         // create rows * columns board
-        for (i = 0; i < rows; i++) {
+        for (let i = 0; i < this.#rows; i++) {
 
             if (type == 'console') {
                 arr.push([]);
-                for (j = 0; j < columns; j++) {
+                for (let j = 0; j < this.#columns; j++) {
                     arr[i].push('');
                 }
             } else if (type == 'dom') {
@@ -24,30 +24,27 @@ const gameBoard = (function () {
         return arr;
     }
 
-    const console = createBoard('console');
-    const dom = createBoard('dom');
+    #console = this.#createBoard('console');
+    #dom = this.#createBoard('dom');
 
-    const getSize = () => {
-        return {rows, columns};
-    }
-
-    const getEmptyCells = () => {
+    #getEmptyCells = () => {
         // checks if any row of the board has empty cells
-        return console.filter( row => row.includes('')).length;
+        return this.#console.filter( row => row.includes('')).length;
     }
 
-    return {
-        console,
-        dom,
-        getEmptyCells,
-        getSize
-    };
-})();
+    constructor() {
+        this.console = this.#console;
+        this.dom = this.#dom;
+        this.getEmptyCells = this.#getEmptyCells;
+        this.rows = this.#rows;
+        this.columns = this.#columns;
+    }
+};
 
-const gameController = (function() {
+class GameController extends GameBoard {
 
     // create players
-    function createPlayer(pseudoName, mark, score = 0) {
+    #createPlayer(pseudoName, mark, score = 0) {
 
         const scoreElement = document.querySelector(`.score.${pseudoName}`);
 
@@ -59,180 +56,177 @@ const gameController = (function() {
     }
 
     // board info
-    const board = gameBoard.console;
-    const domBoard = gameBoard.dom;
-    const boardSize = gameBoard.getSize().columns;
-    const directions = ['row', 'column', 'diagonal-r', 'diagonal-l'];
+    #board = this.console;
+    #domBoard = this.dom;
+    #boardSize = this.rows;
+    #directions = ['row', 'column', 'diagonal-r', 'diagonal-l'];
 
     // DOM
-    const gameContainer = document.querySelector('.game-container');
-    const rectangle = document.querySelector('.game-status .rectangle');
-    const playerName = document.querySelector('span.player-name');
-    const markImg = document.querySelector('img.mark');
-    const menu = document.querySelector('.game-menu');
-    const inputs = document.querySelectorAll('input[type="text"]');
-    const playBtn = document.querySelector('button.play');
-    const endGame = document.querySelector('.end-game');
-    const strike = document.querySelector('.strike');
-    const newGameBtn = document.querySelector('.reset');
-    const newRoundBtn = document.querySelector('.next');
+    #gameContainer = document.querySelector('.game-container');
+    #rectangle = document.querySelector('.game-status .rectangle');
+    #playerName = document.querySelector('span.player-name');
+    #markImg = document.querySelector('img.mark');
+    #menu = document.querySelector('.game-menu');
+    #inputs = document.querySelectorAll('input[type="text"]');
+    #playBtn = document.querySelector('button.play');
+    #endGame = document.querySelector('.end-game');
+    #strike = document.querySelector('.strike');
+    #newGameBtn = document.querySelector('.reset');
+    #newRoundBtn = document.querySelector('.next');
 
     /*  player names are updated after starting the game,
         but we need the objects for the event trigger
     */
-    const player1 = createPlayer('player1', 'x');
-    const player2 = createPlayer('player2', 'o');
+    #player1 = this.#createPlayer('player1', 'x');
+    #player2 = this.#createPlayer('player2', 'o');
 
     // set first player
-    let currPlayer = player1;
-    let roundOver;
+    #currPlayer = this.#player1;
+    #roundOver;
 
-    // add event listeners
-    manageEvent('addEventListener');
-
-    function playRound(row, cell, dom) {
-
-        if (gameBoard.getEmptyCells() == 0) {
+    playRound(row, cell, dom) {
+        if (this.getEmptyCells() == 0) {
             return;
         }
         
         // make the move
-        board[row][cell] = currPlayer.mark;
+        this.#board[row][cell] = this.#currPlayer.mark;
 
         // draw the marks in console and DOM
-        for (arr of board) {
+        for (let arr of this.#board) {
             console.log(arr.join(' | '));
         }
 
         // guard to don't break app when playing from console
         if (dom) {
             const img = document.createElement('img');
-            img.setAttribute('src', `./assets/${currPlayer.mark.toLowerCase()}-icon.svg`);
+            img.setAttribute('src', `./assets/${this.#currPlayer.mark.toLowerCase()}-icon.svg`);
             event.target.appendChild(img);
         }
 
-        checkWinner(row, cell);
-        changeTurn();
+        this.#checkWinner(row, cell);
+        this.#changeTurn();
     }
 
-    function manageEvent(type) {
-
-        // domboard
-        domBoard.forEach( row => {
-            row.forEach( cell => {
-                cell[type]('click', boardHandler);
-            })});
-
-        // input boxes
-        inputs.forEach( input => {
-            input[type]('input', inputHandler);
-        });
-        
-        playBtn[type]('click', playHandler);
-
-        // game over
-        [newGameBtn, newRoundBtn].forEach( selector => selector[type]('click', gameOverHandler));
-
-        function boardHandler(e) {
+    manageEvent(type) {
+        let row, cell;
+        const boardHandler = (e) => {
             // find where the click was done
-            for (i = 0; i < domBoard.length; i++) {
-                if (domBoard[i].includes(e.srcElement)) {
+            for (let i = 0; i < this.#domBoard.length; i++) {
+                if (this.#domBoard[i].includes(e.srcElement)) {
                     row = i;
-                    cell = domBoard[i].indexOf(e.srcElement);
+                    cell = this.#domBoard[i].indexOf(e.srcElement);
                     break;
                 }
             }
 
             // check console board to see if cell is empty
             // @TO DO: use DOM? like e.srcElement.children.length
-            if (board[row][cell] == '' && !roundOver) {
-                playRound(row, cell, true);
+            if (this.#board[row][cell] == '' && !this.#roundOver) {
+                this.playRound(row, cell, true);
             }
         }
 
-        function inputHandler(e) {
-            if (inputs[0].value != '' && inputs[1].value != '') {
-                playBtn.classList.remove('hidden');
+        const inputHandler = (e) => {
+            if (this.#inputs[0].value != '' && this.#inputs[1].value != '') {
+                this.#playBtn.classList.remove('hidden');
             } else {
-                playBtn.classList.add('hidden');
+                this.#playBtn.classList.add('hidden');
             }
         }
 
-        function playHandler(e) {
-            [menu, gameContainer].forEach( item => item.classList.toggle('hidden'));
+        const playHandler = (e) => {
+            [this.#menu, this.#gameContainer].forEach( item => item.classList.toggle('hidden'));
             /*
                 objects gets created before the event trigger occurs,
                 so we need to update the names now, because initial
                 names are plain 'player1' and 'player2'
             */
-            player1.name = inputs[0].value;
-            player2.name = inputs[1].value;
+            this.#player1.name = this.#inputs[0].value;
+            this.#player2.name = this.#inputs[1].value;
 
             // set initial UI for `game-status` @HTML
-            playerName.textContent = player1.name;
-            rectangle.classList.add(player1.pseudoName);
+            this.#playerName.textContent = this.#player1.name;
+            this.#rectangle.classList.add(this.#player1.pseudoName);
             
         }
 
-        function gameOverHandler(e) {
+        const gameOverHandler = (e) => {
             if (e.target.className == 'next') {
-                resetBoard();
+                this.#resetBoard();
             } else {
                 // simply reload the page
                 window.location.reload();
             }
         }
+
+        // domboard
+        this.#domBoard.forEach( row => {
+            row.forEach( cell => {
+                cell[type]('click', boardHandler);
+            })});
+
+        // input boxes
+        this.#inputs.forEach( input => {
+            input[type]('input', inputHandler);
+        });
+        
+        this.#playBtn[type]('click', playHandler);
+
+        // game over
+        [this.#newGameBtn, this.#newRoundBtn].forEach( selector => selector[type]('click', gameOverHandler));
+
     }
 
-    function checkWinner(row, cell) {
+    #checkWinner(row, cell) {
         let combos, markCount, className;
 
-        console.log(`current mark: ${currPlayer.mark}`)
-        for (direction of directions) {
+        console.log(`current mark: ${this.#currPlayer.mark}`)
+        for (let direction of this.#directions) {
             // reset for the new iteration
             combos = '';
             markCount = 0;
             
-            for (i = 0; i < boardSize; i++) {
+            for (let i = 0; i < this.#boardSize; i++) {
                 switch (true) {
                     case (direction == 'row'):
-                        combos += board[row][i];
+                        combos += this.#board[row][i];
                         className = `${direction}-${row + 1}`;
                         break;
                     case (direction == 'column'):
-                        combos += board[i][cell];
+                        combos += this.#board[i][cell];
                         className = `${direction}-${cell + 1}`;
                         break;
                     case (direction.includes('diagonal')):
                         // boardSize - 1 because index starts from 0
-                        combos += (direction == 'diagonal-r') ? board[i][i] : board[i][boardSize - 1 - i];
+                        combos += (direction == 'diagonal-r') ? this.#board[i][i] : this.#board[i][this.#boardSize - 1 - i];
                         className = direction;
                         break;
                     }
             }
 
             // check how many marks are in the current direction
-            markCount = combos.split('').filter( mark => mark == currPlayer.mark).length;
+            markCount = combos.split('').filter( mark => mark == this.#currPlayer.mark).length;
 
             console.log(`combos: ${combos} - direction: ${direction} - markCount: ${markCount}`);
             
-            if (markCount == boardSize) {
-                currPlayer.updateScore();
-                roundOver = true;
+            if (markCount == this.#boardSize) {
+                this.#currPlayer.updateScore();
+                this.#roundOver = true;
 
                 // we need to add 'player1/2' class to choose the color
-                strike.classList.add(currPlayer.pseudoName, className);
+                this.#strike.classList.add(this.#currPlayer.pseudoName, className);
 
-                strike.classList.remove('hidden');
-                endGame.classList.remove('hidden');
+                this.#strike.classList.remove('hidden');
+                this.#endGame.classList.remove('hidden');
 
-                manageEvent('removeEventListener');
+                this.manageEvent('removeEventListener');
                 return;
             }
         }
     }
 
-    function changeTurn() {
+    #changeTurn() {
         /*
             we need to remove the class of currPlayer first,
             and then add the class corresponding to the next
@@ -242,33 +236,34 @@ const gameController = (function() {
                    duplicated code.
         */
 
-        rectangle.classList.remove(currPlayer.pseudoName);
-        currPlayer = (currPlayer == player1) ? player2 : player1;
-        rectangle.classList.add(currPlayer.pseudoName);
+        this.#rectangle.classList.remove(this.#currPlayer.pseudoName);
+        this.#currPlayer = (this.#currPlayer == this.#player1) ? this.#player2 : this.#player1;
+        this.#rectangle.classList.add(this.#currPlayer.pseudoName);
 
         // name node
-        playerName.textContent = currPlayer.name;
+        this.#playerName.textContent = this.#currPlayer.name;
         // mark node
-        markImg.setAttribute('src', `./assets/${currPlayer.mark}-status-icon.svg`);
+        this.#markImg.setAttribute('src', `./assets/${this.#currPlayer.mark}-status-icon.svg`);
     }
 
-    function resetBoard() {
+    #resetBoard() {
 
-        roundOver = false;
+        console.log(this);
+        this.#roundOver = false;
 
         // reset strike's classes
-        strike.classList.value = 'strike hidden';
+        this.#strike.classList.value = 'strike hidden';
 
         // hide/show
-        endGame.classList.add('hidden');
+        this.#endGame.classList.add('hidden');
 
-        board.forEach( row => {
-            for(i = 0; i < gameBoard.getSize().rows; i++) {
+        this.#board.forEach( row => {
+            for(let i = 0; i < this.rows; i++) {
                 row[i] = '';
             }
         });
 
-        domBoard.forEach( row => {
+        this.#domBoard.forEach( row => {
             row.forEach(cell => {
                 if (cell.childNodes.length != 0) {
                     cell.removeChild(cell.childNodes[0]);
@@ -276,6 +271,7 @@ const gameController = (function() {
             });
         })
     }
+};
 
-    return {playRound};
-})();
+const tictactoe = new GameController();
+tictactoe.manageEvent('addEventListener');
